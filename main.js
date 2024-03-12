279c185b40d85105b2e2482d6fdf7337f59038db
JorgeHernndez <163049908+JorgeHernndez@users.noreply.github.com>
2024-03-11 21:12:41 -0500

    ejercicio

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ const funciones=require('./funciones.js');
 const os=require('os');
 
 console.log(funciones.personas);
-setTimeout(funciones.suma,2000,10,30);
+setTimeout(funciones.suma,2,10,30);
 console.log(funciones.saludo());
 console.log(os.platform());
 
-/* console.log("Hola mundo");
+console.log("Hola mundo");
 console.warn("Esta es una advertencia");
 console.assert("Hola"=="Hola","No son iguales");
 console.error(new Error("Este es un error"));
 console.table([1,2,3,4,5]);
+setImmediate(funciones.suma,10,40);
 console.table([{a:1,b:2},{a:3,b:4}]);
-console.table(funciones.personas); */
+console.table(funciones.personas); 
 
 
 /* //const funciones=require('./funciones.js');
